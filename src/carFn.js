c024acabfebd1c72a646e1ae0c572f9ae5e0dcf0
Rebecca Tay <rebeccatay92@gmail.com>
2017-07-11 10:42:57 +0800

    carFn.js object created

--- src/carFn.js
+++ src/carFn.js
@@ -1,6 +1,22 @@
 // Phase I, don't require this file after you're done with Phase I
 
 // Create the object properties and function
-var Car = {}
+var Car = {
+  make: 'Honda',
+  model: 'Vezel',
+  year: 2017,
+  color: 'white',
+  seats: 4,
+  previousOwners: [],
+  owner: 'manufacturer',
+  running: false,
+  sell (name) {
+    this.previousOwners.push(this.owner)
+    this.owner = name
+  },
+  paint (newColor) {
+    this.color = newColor
+  }
+} // close object
 
 // Export the `Car` object
